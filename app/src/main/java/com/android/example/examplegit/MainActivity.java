d11bda4def13ea59c61551062bb0fd308f646501
dawid <dawid2.moskala1@gmail.com>
2018-08-08 19:50:50 +0200

    second

--- app/src/main/java/com/android/example/examplegit/MainActivity.java
+++ app/src/main/java/com/android/example/examplegit/MainActivity.java
@@ -1,7 +1,9 @@
 package com.android.example.examplegit;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button but = (Button) findViewById(R.id.button);
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundColor(Color.GREEN);
+            }
+        });
     }
 }
