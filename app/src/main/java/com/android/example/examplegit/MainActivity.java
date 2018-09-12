1f373c6ce03b84126825edf79bc9cb61ad96dd17
Jodzik1 <dawid2.moskala1@gmail.com>
2018-09-12 20:11:09 +0200

    Update MainActivity.java

--- app/src/main/java/com/android/example/examplegit/MainActivity.java
+++ app/src/main/java/com/android/example/examplegit/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
                  v.setBackgroundColor(Color.BLACK); 
                  v.setBackgroundColor(Color.BLACK); 
                  v.setBackgroundColor(Color.BLACK); 
+                v.setBackgroundColor(Color.BLACK);
+                v.setBackgroundColor(Color.BLACK);
                 
         
             }
